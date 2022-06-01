ae1d3442cbecedea3ed2d34c50f10d95730bebd9
Mark Elliott <markelliottva@gmail.com>
2022-05-31 20:53:37 -0400

    finished user routes, started resume routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const resumeRoutes = require('./resume-routes.js');
-const applicationRoutes = require('./application-routes');
-const companyRoutes = require('./company-routes.js');
-const positionRoutes = require('./position-routes.js');
-const managerRoutes = require('./manager-routes');
+// const applicationRoutes = require('./application-routes');
+// const companyRoutes = require('./company-routes.js');
+// const positionRoutes = require('./position-routes.js');
+// const managerRoutes = require('./manager-routes');
 
 router.use('/users', userRoutes);
-router.use('/resumes', resumeRoutes);
-router.use('/applications', applicationRoutes);
-router.use('/companies', companyRoutes);
-router.use('/positions', positionRoutes);
-router.use('/managers', managerRoutes);
+router.use('/resume', resumeRoutes);
+// router.use('/applications', applicationRoutes);
+// router.use('/companies', companyRoutes);
+// router.use('/positions', positionRoutes);
+// router.use('/managers', managerRoutes);
 
 module.exports = router;
