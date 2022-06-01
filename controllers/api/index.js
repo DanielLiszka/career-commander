4ca1e8c776816029e917a2c6f9e3e40a37224e98
Mark Elliott <markelliottva@gmail.com>
2022-06-01 07:57:15 -0400

    changed models for application queries to work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const resumeRoutes = require('./resume-routes.js');
-// const applicationRoutes = require('./application-routes');
+const applicationRoutes = require('./application-routes');
 // const companyRoutes = require('./company-routes.js');
 // const positionRoutes = require('./position-routes.js');
 // const managerRoutes = require('./manager-routes');
 
 router.use('/users', userRoutes);
 router.use('/resume', resumeRoutes);
-// router.use('/applications', applicationRoutes);
+router.use('/applications', applicationRoutes);
 // router.use('/companies', companyRoutes);
 // router.use('/positions', positionRoutes);
 // router.use('/managers', managerRoutes);
